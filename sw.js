fe9b9399e92be4c78ad5741d1a6257d488f52cef
Jbahamas <141621459+Jbahamas@users.noreply.github.com>
2025-12-29 15:58:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,46 @@
-const CACHE_NAME = 'keja-cache-v1';
+const CACHE_NAME = 'keja-cache-v2'; // Incremented version
 const urlsToCache = [
   '/',
   'index.html',
+  'manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
   'https://i.ibb.co/3mKNG8nV/House-logo.png'
 ];
 
+// Install Event - caching the "shell" of your app
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event - cleaning up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
+// Fetch Event - Network First Strategy
+// This allows Firebase/Cloudinary data to be fresh while keeping the UI offline-capable
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
